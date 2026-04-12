78f17971c0e5c8a8dbb0314d7e608c18b2363a40
J.Garingan <jonathangaringan65@gmail.com>
2026-04-12 15:25:04 +0800

    fetch error and loading

--- src/components/api-consume/ApiConsume.jsx
+++ src/components/api-consume/ApiConsume.jsx
@@ -2,28 +2,36 @@ import React, { useEffect, useState } from "react";
 
 function ApiConsume() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null)
 
   useEffect(() => {
+    setLoading(true);
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
-      .then((json) => setData(json));
+      .then((json) => setData(json), setLoading(false))
+      .catch(error => console.log(`Error: ${error}`), setError(error));
   }, []);
 
+  if (loading) {
+    return <p>Loading......</p>;
+  }
   return (
     <div>
       <h2>Api Consume</h2>
 
-        
-          {/* <p>User ID: {data.userId}</p>
+      {/* <p>User ID: {data.userId}</p>
           <p>Todo ID: {data.id}</p>
           <p>Todo Title{data.title}</p> */}
 
-          {data.map((post)=>(
-            <ul>
-                <li>{post.id}</li>
-                <li><strong>{post.title}</strong></li>
-            </ul>
-          ))}
+      {data.map((post) => (
+        <ul>
+          <li>{post.id}</li>
+          <li>
+            <strong>{post.title}</strong>
+          </li>
+        </ul>
+      ))}
     </div>
   );
 }
