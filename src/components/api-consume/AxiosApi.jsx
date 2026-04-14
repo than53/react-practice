7ca3ef47dae9c72e5d9a937198d74ee794a853ce
J.Garingan <jonathangaringan65@gmail.com>
2026-04-14 11:00:44 +0800

    tried multiple api call in axios

--- src/components/api-consume/AxiosApi.jsx
+++ src/components/api-consume/AxiosApi.jsx
@@ -1,37 +1,52 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react'
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 function AxiosApi() {
-    const [data, setData] = useState([]);
-      const [loading, setLoading] = useState(false);
-      const [error, setError] = useState(null)
-    
-      useEffect(() => {
-        setLoading(true);
-        axios.get("https://jsonplaceholder.typicode.com/posts")
-          .then((response) => setData(response.data), setLoading(false))
-          .catch(error => console.log(`Error: ${error}`), setError(error), setLoading(false));
-      }, []);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .all([
+        axios.get("https://jsonplaceholder.typicode.com/posts"),
+        axios.get("https://jsonplaceholder.typicode.com/users"),
+      ])
+
+      .then(
+        axios.spread((posts, users) => {
+          console.log(posts);
+          console.log(users);
+          (setData(posts.data), setLoading(false));
+        }),
+      )
+      .catch(
+        (error) => console.log(`Error: ${error}`),
+        setError(error),
+        setLoading(false),
+      );
+  }, []);
   return (
     <div>
-         <div>
-      <h2>Api Consume</h2>
+      <div>
+        <h2>Api Consume</h2>
 
-      {/* <p>User ID: {data.userId}</p>
+        {/* <p>User ID: {data.userId}</p>
           <p>Todo ID: {data.id}</p>
           <p>Todo Title{data.title}</p> */}
 
-      {data.map((post) => (
-        <ul>
-          <li>{post.id}</li>
-          <li>
-            <strong>{post.title}</strong>
-          </li>
-        </ul>
-      ))}
+        {data.map((post, index) => (
+          <ul key={index}>
+            <li>{post.id}</li>
+            <li>
+              <strong>{post.title}</strong>
+            </li>
+          </ul>
+        ))}
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default AxiosApi
+export default AxiosApi;
