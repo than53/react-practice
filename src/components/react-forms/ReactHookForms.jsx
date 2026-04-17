8d7418670f7eac173cba9ab165b712b6e7a201f1
J.Garingan <jonathangaringan65@gmail.com>
2026-04-17 21:49:00 +0800

    Apply reset and custom validation in react form

--- src/components/react-forms/ReactHookForms.jsx
+++ src/components/react-forms/ReactHookForms.jsx
@@ -1,4 +1,3 @@
-import React, { useEffect } from "react";
 import "../react-forms/ReactBasicForms.css";
 import { useForm } from "react-hook-form";
 
@@ -7,11 +6,21 @@ function ReactHookForms() {
     register,
     handleSubmit,
     watch,
+    reset,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+  };
   const watchName = watch("name");
   const watchEmail = watch("email");
+
+  const existingUsernames = ["admin", "user123", "john"];
+  const checkIfUsernameExist = async (username) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return existingUsernames.includes(username);
+  };
 
   return (
     <div>
@@ -26,6 +35,15 @@ function ReactHookForms() {
                 value: 2,
                 message: "Name should be alteast 2 characters",
               },
+              validate: {
+                notAdmin: (value) =>
+                  value !== "admin" || "admin is not allowed",
+                isNotNumber: (value) => isNaN(value) || "Name cannot be number",
+                checkUsername: async (value) => {
+                  const exist = await checkIfUsernameExist(value);
+                  return !exist || "Username already taken";
+                },
+              },
             })}
           />
         </label>
@@ -34,7 +52,28 @@ function ReactHookForms() {
           Email:
           <input {...register("email", { required: true })} />
         </label>
-        {errors.emaik && <p>Email is required</p>}
+        {errors.email && <p>Email is required</p>}
+
+        <label>
+          Password:
+          <input
+            type="password"
+            {...register("password", { required: true })}
+          />
+        </label>
+         {errors.password && <p>{errors.password.message}</p>}
+
+        <label>
+          Confirm Password:
+          <input
+            type="password"
+            {...register("confirmPassword", { required: true,
+              validate:(value)=> value ===watch("password") || "Password does not match"
+             })}
+          />
+        </label>
+          {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
+
         <button type="submit">Submit</button>
       </form>
     </div>
